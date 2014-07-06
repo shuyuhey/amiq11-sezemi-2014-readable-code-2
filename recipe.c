0bff598abb36c59109219e1bb504ffd1beb76132
Makoto Shimazu <makoto.shimaz@gmail.com>
2014-07-06 10:56:59 +0900

    spec1: オムライスをprint

---
+++ recipe.c
@@ -0,0 +1,16 @@
+//! gcc -o recipe.bin recipe.c -W -Wall -O3 -std=gnu99 
+/* 
+ * 
+ * Author:   Makoto Shimazu <makoto.shimaz@gmail.com>
+ * URL:      https://amiq11.tumblr.com               
+ * License:  MIT License                             
+ * Created:  2014-07-06                                  
+ *  
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("オムライス\n");
+    return 0;
+}
