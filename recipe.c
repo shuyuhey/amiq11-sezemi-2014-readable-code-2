8125e698faf785332f6cb8581a7fee2081309fe8
Makoto Shimazu <makoto.shimaz@gmail.com>
2014-07-06 11:53:50 +0900

    spec5: Print all recipes with unique IDs

--- recipe.c
+++ recipe.c
@@ -9,24 +9,89 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "recipe.h"
 
 /* params */
 const size_t RECIPE_MAX_LEN = 100;
+const size_t RECIPE_LIST_DEFAULT_SIZE = 1000;
 
 void print_usage(char *path)
 {
     fprintf(stderr, "Usage: %s recipe_path\n", path);
 }
 
-void print_recipe(const char *path)
+inline void *mymalloc(size_t size) {
+    void *tmp = malloc(size);
+    if (tmp == NULL) { perror("malloc"); exit(EXIT_FAILURE); }
+    return tmp;
+}
+
+static recipe_id_t create_new_id()
+{
+    static recipe_id_t latest_id = 0;
+    return latest_id++;
+}
+
+static recipe_t *create_new_recipe(char *name)
+{
+    recipe_t *recipe = (recipe_t *)mymalloc(sizeof(recipe_t));
+    /* init variables */
+    sscanf(name, "%ms", &recipe->name);  /* copy from name to tmp with allocation */
+    recipe->id = create_new_id();
+    return recipe;
+}
+
+
+static recipe_list_t *create_new_recipe_list()
+{
+    recipe_list_t *list = mymalloc(sizeof(recipe_list_t));
+    list->list = mymalloc(sizeof(recipe_t*) * RECIPE_LIST_DEFAULT_SIZE);
+    list->size = 0;
+    list->alloced = RECIPE_LIST_DEFAULT_SIZE;
+    return list;
+}
+
+static bool append_to_recipe_list(recipe_list_t *list, recipe_t *recipe)
+{
+    if (list->size == list->alloced) {
+        size_t new_size = list->alloced * 2;
+        recipe_t *new_list = realloc(list->list, sizeof(recipe_t) * new_size);
+        if (new_list == NULL) { perror("append_to_recipe_list"); exit(EXIT_FAILURE); }
+        list->alloced = new_size;
+    }
+    list->list[list->size] = recipe;
+    list->size ++;
+    return true;
+}
+
+static recipe_list_t *read_recipe_from_file(const char *path)
 {
     /* prepare file */
     FILE *fp = fopen(path, "r");
     if (fp == NULL) { perror("fopen"); exit(EXIT_FAILURE); }
-    char recipe[RECIPE_MAX_LEN];
-    /* print all recipe */
-    while (fgets(recipe, RECIPE_MAX_LEN, fp) != NULL) {
-        printf("%s", recipe);
+    char line[RECIPE_MAX_LEN];
+    recipe_list_t *recipe_list = create_new_recipe_list();
+    
+    /* get all recipe and create a list of recipes */
+    while (fgets(line, RECIPE_MAX_LEN, fp) != NULL) {
+        /* printf("%s", recipe); */
+        recipe_t *recipe = create_new_recipe(line);
+        if (!append_to_recipe_list(recipe_list, recipe)) {
+            fclose(fp);
+            return NULL;
+        }
+    }
+    fclose(fp);
+    return recipe_list;
+}
+
+void print_recipes(const recipe_list_t *list)
+{
+    for (size_t i = 0; i < list->size; i++) {
+        recipe_t *r = list->list[i];
+        printf("%ld: %s\n", r->id, r->name);
     }
 }
 
@@ -39,6 +104,7 @@ int main(int argc, char *argv[])
     }
     /* set args to each variants */
     const char *recipe_path = argv[1];
-    print_recipe(recipe_path);
+    recipe_list_t *recipe_list = read_recipe_from_file(recipe_path);
+    print_recipes(recipe_list);
     return 0;
 }
---
+++ recipe.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdlib.h>
+#include <stdint.h>
+typedef int64_t recipe_id_t;
+typedef struct _recipe
+{
+    recipe_id_t id;
+    char *name;
+} recipe_t;
+
+typedef struct _recipe_list
+{
+    recipe_t **list;
+    size_t size;
+    size_t alloced;
+} recipe_list_t;
